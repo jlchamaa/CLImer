b3a34e8f51937546566abb27363e1c8f40054786
Jean-Luc <jlchamaa@gmail.com>
2016-12-30 13:36:55 -0800

    beginning unittests

---
+++ resources/unitTests.py
@@ -0,0 +1,15 @@
+import unittest
+import dbO
+import sqlite3
+
+class Test_DB_methods(unittest.TestCase):
+
+    def setUp(self):
+        self.db_Instance = dbO.dbO()
+    def test_connection:
+        newCurs = sqlite3
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
